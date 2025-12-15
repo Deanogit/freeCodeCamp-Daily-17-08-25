f31ea8f77bd9f4bd7457674086053ff62720d294
Dean Howe <hideanhowe@gmail.com>
2025-12-15 17:21:48 +0000

    testing

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 // Given an array of numbers and an integer target, find two unique numbers in the array that add up to the target value. Return an array with the indices of those two numbers, or "Target not found" if no two numbers sum up to the target.
 
 // The returned array should have the indices in ascending order.
+
+function findTarget(arr, target) {
+  console.log(arr, target);
+  // loop through arr
+  for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(arr[i]);
+  }
+  // check if a + b = target
+  // // return indicies of a & b
+
+  // return arr;
+}
