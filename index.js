b4dce7d7aced157cd906fca17bb6aae2f9ac99d7
Dean Howe <hideanhowe@gmail.com>
2025-12-16 08:22:13 +0000

    loop solution

--- index.js
+++ index.js
@@ -3,14 +3,42 @@
 
 // The returned array should have the indices in ascending order.
 
-function findTarget(arr, target) {
-  console.log(arr, target);
-  // loop through arr
-  for (let i = 0; i <= arr.length - 1; i++) {
-    console.log(arr[i]);
-  }
-  // check if a + b = target
-  // // return indicies of a & b
+// function findTarget(arr, target) {
+// console.log(arr, target)
+// loop through arr
 
-  // return arr;
+//for (let i = 0; i <= arr.length -1 ; i ++) {
+//  console.log(arr[i])
+// }
+
+// arr.forEach((x,i) => {
+//   console.log(x, i)
+//   if (arr[i] + arr[i + 1] === target) {
+//     console.log("Match!")
+//     return [i, i + 1]
+//   } else {
+//     return "Target not found"
+//   }
+// })
+// check if a + b = target
+// // return indicies of a & b
+// return arr;
+//}
+
+function findTarget(arr, target) {
+  const result = [];
+  console.log(arr, target);
+  for (let i = 0; i <= arr.length - 1; i++) {
+    for (let x = i + 1; x <= arr.length - 1; x++) {
+      // console.log(i, x)
+      console.log(arr[i], arr[x]);
+      if (arr[i] + arr[x] === target) {
+        console.log('Match!');
+        result.push(i, x);
+        return result;
+      }
+    }
+  }
+  console.log(result);
+  return 'Target not found';
 }
